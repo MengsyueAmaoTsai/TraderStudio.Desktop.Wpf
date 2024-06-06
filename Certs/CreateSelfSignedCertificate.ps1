a02f39e270ba34330404aaab544631b5289e8a14
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-06 22:51:59 +0800

    update powershell

--- Certs/CreateSelfSignedCertificate.ps1
+++ Certs/CreateSelfSignedCertificate.ps1
@@ -1,7 +1,12 @@
 $currentDate = Get-Date
 $extendedDate = $currentDate.AddYears(3)
-$cert = New-SelfSignedCertificate -Subject "CN-RichillCapital" -CertStoreLocation "Cert:\CurrentUser\My" -DnsName richillcapital.trader-studio-desktop -NotAfter $extendedDate -KeyUsage DigitalSignature -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
-$password = ConvertTo-SecureString -String Pa55w0rd! -Force -AsPlainText
-$path = "Cert:\CurrentUser\My\" + $cert.thumbprint
+$subject = "CN=RichillCapital"
+$location = "Cert:\CurrentUser\My"
+$dnsName = "richillcapital.trader-studio-desktop"
+$output = "./Certs/RichillCapital.TraderStudio.Desktop.pfx"
 
-Export-PfxCertificate -cert $path -FilePath ./Certs/RichillCapital.TraderStudio.Desktop.pfx -Password $password
+$cert = New-SelfSignedCertificate -Subject $subject -CertStoreLocation $location -DnsName $dnsName -NotAfter $extendedDate -KeyUsage DigitalSignature -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
+$password = ConvertTo-SecureString -String Pa55w0rd! -Force -AsPlainText
+$path = $location + "/" + $cert.thumbprint
+
+Export-PfxCertificate -cert $path -FilePath $output -Password $password
--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## Build
 
 ```bash
-msbuild .\RichillCapital.TraderStudio.Desktop.sln /p:Platform=x64 /p:Configuration=Release /p:UapAppxPackageBuildMode=SideloadOnly /p:AppxBundle=Never
+msbuild .\RichillCapital.TraderStudio.Desktop.sln /p:Platform=x64 /p:Configuration=Release /p:UapAppxPackageBuildMode=SideloadOnly /p:AppxBundle=Never /p:PackageCertificateKeyFile=Certs/RichillCapital.TraderStudio.Desktop.pfx
 ```
