f85732f393f91eed860c47b036269e3ccba544f1
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-07 03:08:06 +0800

    show pacakge information

---
+++ Src/ApplicationInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+
+namespace RichillCapital.TraderStudio.Desktop;
+
+internal class ApplicationInfo
+{
+    private const string NotPackaged = "Not packaged";
+
+    internal static string GetDisplayName()
+    {
+        return NotPackaged;
+    }
+
+    internal static string GetAssemblyVersion()
+    {
+        var attribute = typeof(App).Assembly
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>() ??
+            throw new InvalidOperationException();
+
+        return attribute.InformationalVersion;
+    }
+
+    internal static string GetPackageVersion()
+    {
+        return NotPackaged;
+    }
+
+    internal static string GetAppInstallerUri()
+    {
+        return string.Empty;
+    }
+
+    internal static string GetInstallLocation() => Assembly.GetExecutingAssembly().Location;
+
+    internal static string GetDotNetRuntimeInfo()
+    {
+        var attribute = typeof(object).Assembly
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>() ??
+            throw new InvalidOperationException();
+
+        return attribute.InformationalVersion;
+    }
+}
--- Src/MainViewModel.cs
+++ Src/MainViewModel.cs
@@ -1,7 +1,42 @@
+using System.Reflection;
+using System.Windows;
+
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace RichillCapital.TraderStudio.Desktop;
 
 public sealed partial class MainViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private string _displayName;
+
+    [ObservableProperty]
+    private string _assemblyVersion;
+
+    [ObservableProperty]
+    private string _packageVersion;
+
+    [ObservableProperty]
+    private string _appInstallerUri;
+
+    [ObservableProperty]
+    private string _installLocation;
+
+    public MainViewModel()
+    {
+        _displayName = ApplicationInfo.GetDisplayName();
+        _assemblyVersion = ApplicationInfo.GetAssemblyVersion();
+        _packageVersion = ApplicationInfo.GetPackageVersion();
+        _appInstallerUri = ApplicationInfo.GetAppInstallerUri();
+        _installLocation = ApplicationInfo.GetInstallLocation();
+    }
+
+    [RelayCommand]
+    private void ShowRuntimeInfo()
+    {
+        var message = ApplicationInfo.GetDotNetRuntimeInfo();
+
+        MessageBox.Show(message, "Runtime Information", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
 }
