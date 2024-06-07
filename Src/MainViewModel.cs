74e26ed7aacf1a655004ea1e4cedbfdc5f72dcfd
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-08 05:06:30 +0800

    add signal source binding

--- Src/MainViewModel.cs
+++ Src/MainViewModel.cs
@@ -1,3 +1,6 @@
+using System.Collections.ObjectModel;
+using System.Windows;
+
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -9,9 +12,21 @@ public sealed partial class MainViewModel : ObservableObject
 {
     private readonly IServiceProvider _serviceProvider;
 
+    public ObservableCollection<SignalSourceModel> SignalSources { get; } = [];
+
+    [ObservableProperty]
+    private SignalSourceModel? _selectedSignalSource;
+
     public MainViewModel(IServiceProvider serviceProvider)
     {
         _serviceProvider = serviceProvider;
+
+        SignalSources.Add(new SignalSourceModel
+        {
+            Id = "1",
+            Name = "Signal Source 1",
+            Description = "Signal Source 1 Description"
+        });
     }
 
     [RelayCommand]
@@ -21,4 +36,19 @@ public sealed partial class MainViewModel : ObservableObject
 
         dialog.ShowDialog();
     }
+
+    [RelayCommand]
+    private void OpenCreateDialog()
+    {
+        MessageBox.Show($"Selected signal source: {SelectedSignalSource}");
+    }
+}
+
+public sealed record SignalSourceModel
+{
+    public required string Id { get; init; }
+
+    public required string Name { get; init; }
+
+    public required string Description { get; init; }
 }
