9196559823600d60a847adeed5189d03a814945c
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-11-15 06:12:45 +0800

    Form validation

--- src/RichillCapital.TraderStudio.Desktop/ViewModels/NewSignalSourceViewModel.cs
+++ src/RichillCapital.TraderStudio.Desktop/ViewModels/NewSignalSourceViewModel.cs
@@ -1,6 +1,10 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Windows;
+
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
+using RichillCapital.SharedKernel;
+using RichillCapital.SharedKernel.Monads;
 using RichillCapital.TraderStudio.Desktop.Services;
 using RichillCapital.TraderStudio.Domain;
 
@@ -32,10 +36,35 @@ public sealed partial class NewSignalSourceViewModel : ViewModel
     [RelayCommand]
     private void Submit()
     {
+        var maybeError = ValidateForm();
+
+        if (maybeError.HasValue)
+        {
+            MessageBox.Show($"{maybeError.Value.Code} - {maybeError.Value.Message}");
+            return;
+        }
+
+        // Create new signal source with some service
+        MessageBox.Show($"Signal source '{Id}' created successfully.");
     }
 
     [RelayCommand]
     private void Cancel()
     {
+    }
+
+    private Maybe<Error> ValidateForm()
+    {
+        if (string.IsNullOrEmpty(Id))
+        {
+            return Maybe<Error>.With(Error.Invalid("Id is required"));
+        }
+
+        if (string.IsNullOrEmpty(Name))
+        {
+            return Maybe<Error>.With(Error.Invalid("Name is required"));
+        }
+
+        return Maybe<Error>.Null;
     }
 }
